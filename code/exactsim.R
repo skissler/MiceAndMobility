ac06962b6f0860555e51c12975f887c6d38b5303
Stephen Kissler <skissler@hsph.harvard.edu>
2023-03-01 15:13:19 -0500

    added comments

--- code/exactsim.R
+++ code/exactsim.R
@@ -2,48 +2,57 @@
 # Initial definitions
 # =============================================================================
 
+# Import packages:
 library(tidyverse) 
 library(purrr)
 
+# Set epidemiological parameters:
 N <- 1000
 beta <- 1/3
 gamma <- 1/5
 
+# Draw infectious periods for each person: 
 tauistar <- rexp(N, gamma)
 
 # =============================================================================
 # Simulate
 # =============================================================================
 
-# source('code/exactsim.R'); tinfvec[tinfvec<Inf]
-
+# Set a max number of days for simulation
 tmax <- 100
 
+# Initialize vector to track infection times 
 tinfvec <- rep(Inf,N)
-
+# Initialize the first infection
 tinfvec[1] <- 0
 
+# Start at time t = 0, and the next infection will go into position 2
 t <- 0
 infcounter <- 2 
 
 while(t < tmax){
+	# How much time is remaining in each person's infectious period? 
 	infremaining <- tauistar - (t - tinfvec)
-	delta <- min(infremaining[infremaining>0.000001]) # if Inf, stop sim
-	if(delta==Inf){
+	# Find the minimum remaining infectiousness (adjust for machine precision)
+	delta <- min(infremaining[infremaining>0.000001]) 
+	# If delta = Inf, no one is infectious anymore 
+	if(delta==Inf){ 
 		break()
 	}
 
-	# How many people are currently contributing to infectiousness? 
-	# That's people who have been infected and not yet recovered: 
+	# Count how many people are currently infectious:
 	ninf <- sum((tinfvec < Inf) & (tauistar > t-tinfvec))
 
 	# Calculate the total force over the time period from these people: 
 	Ftot <- ninf*beta*delta
+
+	# Calculate the probability of inection for each susceptible person:
 	pinf <- 1-exp(-Ftot/N)
 
+	# Draw the number of new infections
 	newinf <- rbinom(1,N-sum(tinfvec<Inf),pinf)
 
-	# Draw times for the new infections: (add clause where if there are no infections we skip this and set t <- t+delta) 
+	# If there were new infections, draw their times and take the min: 
 	if(newinf > 0){
 		t <- t + min(runif(newinf))*delta
 		tinfvec[infcounter] <- t
@@ -53,15 +62,15 @@ while(t < tmax){
 	}
 }
 
-
+# Collect infection times into a table: 
 tinfdf <- tibble(tinf=tinfvec, taui=tauistar) %>% 
 	mutate(trec=tinf+taui)
-
 plottimes <- seq(from=0,to=tmax,by=1)
 plotinfs <- lapply(plottimes, function(x){
 	return(nrow(tinfdf %>% filter(tinf<=x & trec>x)))
 	}) %>% unlist()
 
+# Plot number of people infectious over time: 
 fig_icurve <- tibble(t=plottimes,inf=plotinfs) %>% 
 	ggplot(aes(x=t, y=inf)) +
 		geom_line() + 
